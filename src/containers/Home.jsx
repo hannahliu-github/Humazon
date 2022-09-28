709c08cf463b6cc2242b67ada094cc3e2c381f1e
hannahliu-github <shhan.liu@gmail.com>
2022-09-27 22:17:47 -0400

    styling homepage and also add StateProvider

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 //import Product from '../components/Product'
-//import '../style/Home.css'
+import '../style/Home.css'
 import { Link } from 'react-router-dom';
 
 function Home() { 
