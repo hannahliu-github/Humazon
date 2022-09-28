e741422a374796f919fa36eb3221edd15201cc49
hannahliu-github <shhan.liu@gmail.com>
2022-09-27 21:54:46 -0400

    Initial Homepage

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import './App.css';
 import { BrowserRouter as  Router, Route, Routes } from "react-router-dom";
 
 import Login from './containers/Login';
+import Home from './containers/Home';
 
 function App() {
   return ( 
   <Router>  
     <div className="app">
     <Routes>
+
+      <Route exact path="/" element={<Home/>} /> 
       <Route exact path="/login" element={<Login/>} />
+         
     </Routes>
     </div>
   </Router>
---
+++ src/containers/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+//import Product from '../components/Product'
+//import '../style/Home.css'
+import { Link } from 'react-router-dom';
+
+function Home() { 
+    return (
+        <div className='home'>
+            <div className='home-container'>
+                <img id='home' className='home-img' src='https://images-na.ssl-images-amazon.com/images/G/01/em/pd21/xcm_em_prime_day_2021_846-usen_d_hp_tallhero_smb_3000x1200._CB668775095_.jpg' alt='background' />
+                <div className='home-row'>
+                    Row_1
+                </div>
+                <div className='home-row'>
+                    Row_2                   
+                </div>
+                <div className='home-row'>
+                    Row_3                   
+                </div>
+                <a href="/">
+                    <div className='backtotop-btn'>
+                        <span>Back to top</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Home
